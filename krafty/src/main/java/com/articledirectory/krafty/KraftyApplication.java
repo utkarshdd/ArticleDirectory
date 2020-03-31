58a26f855973df4ba26b8eca02e505a662688d6e
utkarshdd <utkarshchowdhary4140@gmail.com>
2020-03-31 18:28:27 +0530

    ArticleDirectory BackEnd

---
+++ krafty/src/main/java/com/articledirectory/krafty/KraftyApplication.java
@@ -0,0 +1,13 @@
+package com.articledirectory.krafty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraftyApplication.class, args);
+	}
+
+}
